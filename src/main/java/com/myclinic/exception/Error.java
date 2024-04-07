bc511772be58784134ff1babf130eeb1e4e36cb8
RafaelCMira <113689920+RafaelCMira@users.noreply.github.com>
2024-04-07 23:58:50 +0100

    Simplified Error and ErrorDTO class

--- src/main/java/com/myclinic/exception/Error.java
+++ src/main/java/com/myclinic/exception/Error.java
@@ -1,13 +1,10 @@
 package com.myclinic.exception;
 
-import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Error {
-    private String field;
-    private String input;
-    private String errorMessage;
+record Error(
+        String field,
+        String input,
+        String errorMessage
+) {
+
 }
--- src/main/java/com/myclinic/exception/ErrorDTO.java
+++ src/main/java/com/myclinic/exception/ErrorDTO.java
@@ -1,21 +1,17 @@
 package com.myclinic.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.*;
 
 import java.util.List;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ErrorDTO {
-    private String path;
-    private int httpCode;
-    private String timestamp;
-
-    private List<Error> errors;
-
-    private String message;
+record ErrorDTO(
+        String path,
+        int httpCode,
+        String timestamp,
+        List<Error> errors,
+        String message
+) {
 }
+
